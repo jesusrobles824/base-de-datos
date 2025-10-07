8b43e49bb760cbb4cd2042096f9ca3d0289c8c67
jesus <jesusrobles3500@gmail.com>
2025-10-07 06:00:29 -0300

    Update Variación_Reservas_Factores_Explicación.py

--- Variación_Reservas_Factores_Explicación.py
+++ Variación_Reservas_Factores_Explicación.py
@@ -14,7 +14,7 @@ from io import BytesIO
 url = "https://www.bcra.gob.ar/Pdfs/PublicacionesEstadisticas/series.xlsm"
 response = requests.get(url, verify=False)
 Variacion_Reservas = pd.read_excel(BytesIO(response.content), sheet_name='RESERVAS', skiprows=8, usecols='A,G,H,I,J,K,L,Q')
-Variacion_Reservas.columns = ['fecha', 'Reservas Internacionales', 'Compra de Divisas', 'OOII', 'Otras Operaciones del SP', 'Efectivo Mínimo', 'Otros (incl. pases pasivos en USD con el exterior)', 'Tipo de serie']
+Variacion_Reservas.columns = ['fecha', 'Reservas Internacionales', 'Compra de Divisas', 'OOII', 'Otras Operaciones del Sector Público', 'Efectivo Mínimo', 'Otros (incl. pases pasivos en USD con el exterior)', 'Tipo de serie']
 Variacion_Reservas = Variacion_Reservas.loc[Variacion_Reservas["Tipo de serie"] == "D"]
 Variacion_Reservas.set_index('fecha', inplace=True)
 Variacion_Reservas = Variacion_Reservas.drop(columns=['Tipo de serie'])
@@ -29,3 +29,4 @@ Variacion_Acumulada = Variacion_Acumulada.round(1)
 Variacion_Acumulada = Variacion_Acumulada[::-1]
 
 Variacion_Acumulada.to_csv('Variación_Acumulada_RRII_Factores_Explicación.csv',index=True)
+
